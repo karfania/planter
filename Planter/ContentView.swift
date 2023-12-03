e54c90d1c5687760b73ed471814f7780ef23c25b
karfania <arfania@usc.edu>
2023-12-02 18:23:10 -0800

    Initial Commit

---
+++ Planter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planter
+//
+//  Created by Kory Arfania on 12/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planter/PlanterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanterApp.swift
+//  Planter
+//
+//  Created by Kory Arfania on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
